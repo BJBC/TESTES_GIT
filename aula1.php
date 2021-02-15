77aacce205010a69e403c648985ddf3db877d139
BJBC <bcaldasbc@gmail.com>
2021-02-14 22:36:32 -0300

    NOVO

--- aula1.php
+++ aula1.php
@@ -1,26 +1,32 @@
 <?php
 
 //variaveis
-$anoNovo = "";
+$anoNovo = "2021";//string
+echo($anoNovo);
 
-$nome = "João";
+$anoVelho = 2020;
+echo($anoVelho);
+
+echo "<br/>";
+echo "<br/>";
+
+$nome = "Joao";
 $sobrenome = "Rocha";
 $completo = $nome.$sobrenome;//concatenacao
 echo($completo);//JoãoRocha
-$completo = "$nome" " "."$sobrenome;
+
+echo "<br/>";//nova linha
+echo "<br/>";
+
+//concatenação
+$completo = "$nome" ." "."$sobrenome";
 echo($completo);//João Rocha
 
-echo "<br/>";//quebra de linha
+//exit;//o php para a execução
 
-exit;//o php para a execução
+//unset($nome);//limpar a variável
 
-unset($nome);//limpar a variável
-if (isset($nome){//se $nome existe
- echo($nome);
-}
-
-
-
-
+//if (isset($nome){//se $nome existe
+// echo($nome);}
 
 ?>
